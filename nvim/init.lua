2f891ad89a20b05ca0c184af4cd5eea67a7a1d38
Samael Lovecraft <samael69lovecraft@gmail.com>
2024-06-14 02:32:46 +0300

    Add python3 provider

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,11 @@
 require("base")
 
+if jit.os == "Linux" then
+    vim.g.python3_host_prog = vim.fn.expand("~/.pyenv/versions/py3nvim/bin/python")
+else
+    vim.g.python3_host_prog = vim.fn.expand("~/.pyenv/pyenv-win/versions/py3nvim/Scripts/python")
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
