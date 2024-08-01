1e21a6a3e6d31446380f2ac38ecc7265a6fa2be5
Samael Lovecraft <samael69lovecraft@gmail.com>
2024-08-02 01:27:57 +0300

    nvim: Upgrade cmp config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -10,23 +10,51 @@ return {
         'hrsh7th/nvim-cmp',
         dependencies = {
             'hrsh7th/cmp-nvim-lsp', -- setup in `lsp.lua`
+            'hrsh7th/cmp-nvim-lua',
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
-            'hrsh7th/cmp-cmdline'
+            'hrsh7th/cmp-cmdline',
         },
         config = function()
-            -- friendly-snippets
             require("luasnip.loaders.from_vscode").lazy_load()
 
             local cmp = require('cmp')
-            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            local luasnip = require('luasnip')
+            local kind_icons = {
+                Text = "",
+                Method = "󰆧",
+                Function = "󰊕",
+                Constructor = "",
+                Field = "󰇽",
+                Variable = "󰂡",
+                Class = "󰠱",
+                Interface = "",
+                Module = "",
+                Property = "󰜢",
+                Unit = "",
+                Value = "󰎠",
+                Enum = "",
+                Keyword = "󰌋",
+                Snippet = "",
+                Color = "󰏘",
+                File = "󰈙",
+                Reference = "",
+                Folder = "󰉋",
+                EnumMember = "",
+                Constant = "󰏿",
+                Struct = "",
+                Event = "",
+                Operator = "󰆕",
+                TypeParameter = "󰅲",
+            }
 
             cmp.setup({
+                experimental = {
+                    -- ghost_text = true,
+                },
                 snippet = {
-                    -- REQUIRED - you must specify a snippet engine
                     expand = function(args)
-                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+                        luasnip.lsp_expand(args.body)
                     end,
                 },
                 window = {
@@ -34,21 +62,69 @@ return {
                     -- documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-                    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+                    ['<CR>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            if luasnip.expandable() then
+                                luasnip.expand()
+                            else
+                                cmp.confirm({
+                                    select = true,
+                                })
+                            end
+                        else
+                            fallback()
+                        end
+                    end),
+
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
                 }),
                 sources = cmp.config.sources({
+                    { name = 'nvim_lua' },
                     { name = 'nvim_lsp' },
                     { name = 'luasnip', keyword_length = 2 },
                 }, {
-                        { name = 'buffer', keyword_length = 3 }, -- text within the current file
-                        { name = 'path' }, -- file path completions
-                    })
+                        { name = 'buffer', keyword_length = 5 },
+                        { name = 'path' },
+                    }
+                ),
+                formatting = {
+                    format = function(entry, vim_item)
+                        -- Kind icons
+                        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+                        -- Source
+                        vim_item.menu = ({
+                            nvim_lua = '[lua]',
+                            nvim_lsp = '[LSP]',
+                            luasnip = '[snip]',
+                            buffer = '[buff]',
+                            path = '[path]'
+                        })[entry.source.name]
+                        return vim_item
+                    end
+                }
             })
         end
     }
