26223e7d9716dc15a50ab84598cf19a5cea0cb05
Samael Lovecraft <samael69lovecraft@gmail.com>
2024-08-02 01:30:28 +0300

    nvim: Add comment plugins

---
+++ nvim/lua/plugins/comments.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        'numToStr/Comment.nvim',
+        config = true
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = true
+    }
+}
